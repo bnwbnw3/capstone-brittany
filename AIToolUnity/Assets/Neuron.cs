9f160f54435f4c2b7684cc40c6c576b3e2fb4e7a
Brittany Waite <bnwbnw3@gmail.com>
2014-10-01 10:44:36 -0600

    Creating classes for a neural network

---
+++ AIToolUnity/Assets/Neuron.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Neuron
+{
+    int _numInputs;
+    List<Double> weights;
+
+    public Neuron(int numInputs)
+	{
+        //+1 for bias
+        _numInputs = numInputs+1;
+        weights = new List<Double>();
+
+        //initialize weights to be random.
+        for (int i = 0; i < numInputs + 1; i++)
+        {
+            weights.Add( new Random().NextDouble());
+        }
+
+	}
+
+    public List<Double> getWeights()
+    {
+        return weights;
+    }
+
+    public int getNumInputs()
+    {
+        return _numInputs;
+    }
+
+    public int getNumWeights()
+    {
+        return weights.Count;
+    }
+
+}
