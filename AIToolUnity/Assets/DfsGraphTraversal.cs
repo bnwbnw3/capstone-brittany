c7373eecd717d8ac0436801011128e48292822e4
Brittany Waite <bnwbnw3@gmail.com>
2014-10-05 23:06:17 -0600

    brought in Algo stuff to try to help find out which nodes to give to user after being given a graph of every node that exists

---
+++ AIToolUnity/Assets/DfsGraphTraversal.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class DfsGraphTraversal
+{
+    const int VISITED = 1;
+    const int UNVISITED = 0;
+
+   // This class performs a traversal of a Graph and emits the visit order of the graph’s vertices.
+
+    // The outer list represents all the connected components.
+   public List<List<int>> traverse(Graph g)
+   {
+       List<List<int>> ret = new List<List<int>>();
+
+       for(int vertex = 0; vertex < g.vCount(); vertex++)
+       {
+           if(g.getMark(vertex) == UNVISITED)
+           {
+               List<int> currentTraversal = new List<int>();
+               traverseHelper(currentTraversal, g, vertex);
+               ret.Add(currentTraversal);
+           }
+       }
+       return ret;
+   }
+
+    // The inner List represents a traversal of a connected component in the graph.
+    private void traverseHelper(List<int> aConnectedComponent, Graph g, int vertex)
+    {
+        //PreVisit
+        g.setMark(vertex, VISITED);
+        for (int w = g.first(vertex); w < g.vCount() ; w = g.next(vertex, w))
+        {
+            if (g.getMark(w) == UNVISITED)
+            {
+                traverseHelper(aConnectedComponent, g, w);
+            }
+        }
+        //PostVisit
+        aConnectedComponent.Add(vertex);
+    }
+}
---
+++ AIToolUnity/Assets/TopologicalSort.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class TopologicalSort
+{
+    DfsGraphTraversal DFS;
+    public TopologicalSort()
+    {
+        DFS = new DfsGraphTraversal();
+    }
+   public List<int> sort(Graph g)
+    {
+        return sortWithDFS(g);
+    }
+
+    List<int> sortWithDFS(Graph g)
+    {
+        List<List<int>> backwardsSort = DFS.traverse(g);
+        List<int> sorted = new List<int>();
+
+        for(int i = backwardsSort.Count; i >= 0 ; i--)
+        {
+            for( int j = backwardsSort[i].Count-1; j >= 0  ; j--)
+            {
+                sorted.Add(backwardsSort[i][j]);
+            }
+        }
+        return sorted;
+    }
+
+    List<int> sortWithSourceRemoval(Graph g)
+    {
+        return null;
+    }
+
+    List<int> sourceRemovalHelper( Graph g, int[] vertsLeft)
+    {
+       return null;
+    }
+    }
