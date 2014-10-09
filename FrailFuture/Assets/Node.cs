75d8d2241355a93381fa23eb9b4d325b5f8c3010
Brittany Waite <bnwbnw3@gmail.com>
2014-10-09 15:18:55 -0600

    Create Node and Node Manger for the maze. Have not added them in yet. Just have a single prefab ready for placing

---
+++ FrailFuture/Assets/Node.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+
+public class Node : MonoBehaviour {
+
+    public List<Node> Edges_Connections;
+    public Material openDoorMaterial;
+    public Material closedDoorMaterial;
+    private int index;
+
+    void Awake()
+    {
+        this.renderer.material = openDoorMaterial;
+    }
+	// Use this for initialization
+	void Start () 
+    {
+        index = NodeManager.nodeManager.AllNodes.IndexOf(this);
+        if (index == -1)
+        {
+            //make sure this node is in Node Manager. 
+            //Also make sure this nodes children are added to it.
+            throw new UnassignedReferenceException("Make sure this node is in Node Manager./nAlso make sure this nodes children are added to it.");
+        }
+        Debug.Log("Node" + getIndexFromNodeManager() + " edge Nodes: ");
+        for (int i = 0; i < Edges_Connections.Count; i++)
+        {
+            Debug.Log("Node" + Edges_Connections[i].getIndexFromNodeManager());
+        }
+	}
+
+    // Update is called once per frame
+    void Update() 
+    {
+	}
+
+    public void OnTriggerEnter(Collider c)
+    {
+        renderer.material = closedDoorMaterial;
+        collider.isTrigger = false;
+    }
+    public int getIndexFromNodeManager()
+    {
+        return index;
+    }
+}
+
+
+public class ReadOnlyAttribute : PropertyAttribute
+{
+
+}
+
+[CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+public class ReadOnlyDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position,SerializedProperty property,GUIContent label)
+    {
+        GUI.enabled = false;
+        EditorGUI.PropertyField(position, property, label, true);
+        GUI.enabled = true;
+    }
+}
---
+++ FrailFuture/Assets/NodeManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class NodeManager : MonoBehaviour 
+{
+
+    //0 = start node
+    //last = very last end node
+    public List<Node> AllNodes;
+    public static NodeManager nodeManager;
+	// Use this for initialization
+    void Awake()
+    {
+        if (nodeManager == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            nodeManager = this;
+        }
+        else if (nodeManager != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+}
