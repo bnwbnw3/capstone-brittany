45d8e455883d791e077502d33e6b804c21773116
Brittany Waite <bnwbnw3@gmail.com>
2014-10-09 21:20:09 -0600

    Nodes are now separate from paths. Paths hold the number the user picks while nodes are the rooms. Both are blocks that cover doorways and "close" when the player goes through them

---
+++ FrailFuture/Assets/PathChoice.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+
+public class PathChoice : MonoBehaviour
+{
+    public Material openDoorMaterial;
+    public Material closedDoorMaterial;
+    public int pathNum;
+    private int index;
+
+    void Awake()
+    {
+        this.renderer.material = openDoorMaterial;
+    }
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void OnTriggerEnter(Collider c)
+    {
+        if (collider.isTrigger)
+        {
+            renderer.material = closedDoorMaterial;
+            collider.isTrigger = false;
+        }
+    }
+    public void resetBackToPathTrigger()
+    {
+        renderer.material = openDoorMaterial;
+        collider.isTrigger = true;
+    }
+}
