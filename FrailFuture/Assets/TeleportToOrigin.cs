33c1a29e29e97e05f0ed599e94aeaa74864b73ab
Brittany Waite <bnwbnw3@gmail.com>
2014-10-19 05:03:12 -0600

    Creating new prefabs to work with.

---
+++ FrailFuture/Assets/TeleportToOrigin.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeleportToOrigin : MonoBehaviour {
+
+    static private Vector3 origin;
+
+    void Awake()
+    {
+        origin.x = origin.y = origin.z = 0;
+    }
+    public void OnTriggerEnter(Collider c)
+    {
+        if (c.tag == "Player")
+        {
+            c.transform.position = origin;
+        }
+    }
+}
