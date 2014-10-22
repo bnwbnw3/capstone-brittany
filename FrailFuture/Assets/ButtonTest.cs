e5f9636b28411643add10c30036a2a40e2df3b88
Brittany Waite <bnwbnw3@gmail.com>
2014-10-22 14:33:27 -0600

    Testing/Demo working with new GUI in Unity

---
+++ FrailFuture/Assets/ButtonTest.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ButtonTest : MonoBehaviour 
+{
+    public Slider Slider;
+
+    //To be used from a button click:
+    /// <summary>
+    /// Must be public
+    /// return void
+    /// have 0 to 1 params
+    /// </summary>
+    public void DoSomething()
+    {
+        Debug.Log("Slider value: "+ Slider.value);
+    }
+
+    public void DoSomethingWithASlider(Slider slider)
+    {
+        Debug.Log(slider.value.ToString());
+    }
+	
+}
