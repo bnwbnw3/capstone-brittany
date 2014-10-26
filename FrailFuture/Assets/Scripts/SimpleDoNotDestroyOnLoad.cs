9d3edeb7ea457c3b5cd7fd3595a3b2c322571e2b
Brittany Waite <bnwbnw3@gmail.com>
2014-10-25 22:16:24 -0600

    updating Main Menu with opening and loading scenes. Also made it so theme music plays continuously from Main Menu to Game Scene

---
+++ FrailFuture/Assets/Scripts/SimpleDoNotDestroyOnLoad.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleDoNotDestroyOnLoad : MonoBehaviour {
+
+    public static GameObject self;
+
+    void Awake()
+    {
+        //Edit script Awake call through Edit->ProjectSettings->Script Order
+        if (self == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            self = gameObject;
+        }
+        else if (self != gameObject)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
