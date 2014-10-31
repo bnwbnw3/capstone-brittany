5bbce8db8d46ad51b929150631aeca5fa8d9f8e3
Brittany Waite <bnwbnw3@gmail.com>
2014-10-31 01:14:15 -0600

    More cleaning up of files. Also trying to make GUI have sound for buttons clicks

---
+++ FrailFuture/Assets/Scripts/UISound.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UISound : MonoBehaviour {
+
+    public AudioClip buttonClick;
+    public void playClick()
+    {
+        audio.PlayOneShot(buttonClick);
+    }
+}
