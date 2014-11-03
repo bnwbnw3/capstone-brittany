fbf2344e4bc6f098b3feb31aa5470bb2b98842f6
Brittany Waite <bnwbnw3@gmail.com>
2014-11-03 00:11:30 -0700

    fixing double trigger issue

--- FrailFuture/Assets/Scripts/HallwayDialogueVOTrigger.cs
+++ FrailFuture/Assets/Scripts/HallwayDialogueVOTrigger.cs
@@ -3,29 +3,55 @@ using System.Collections;
 
 public class HallwayDialogueVOTrigger : MonoBehaviour {
 
-    private int lastVertexKnown;
+    private static int lastVertexKnown;
 
     void Awake()
     {
         lastVertexKnown = -1;
         if (GameControl.control.wasLoaded)
         {
+            destroyLargeTrigger();
+        }
+        else
+        {
             string name = this.gameObject.name;
-            if (name.CompareTo("VOIntroTrigger") == 0)
+            if (name.CompareTo("VOTrigger") == 0)
             {
-                Destroy(this.gameObject);
+                this.enabled = false;
             }
         }
     }
+    public void Update()
+    {
+        if (NodeManager.nodeManager.justReset)
+        {
+            destroyLargeTrigger();
+        }
+        if (lastVertexKnown != -1)
+        {
+            this.enabled = true;
+        }
+    }
+
     public void OnTriggerEnter(Collider c)
     {
         if (lastVertexKnown != GameControl.control.Ai.getCurrentGraphIndex() || NodeManager.nodeManager.justReset)
         {
             if (!SoundManager.soundManager.getIsAiTalking())
             {
+                destroyLargeTrigger();
                 lastVertexKnown = GameControl.control.Ai.getCurrentGraphIndex();
                 SoundManager.soundManager.playVODialogue();
             }
         }
+    }
+
+    public void destroyLargeTrigger()
+    {
+        string name = this.gameObject.name;
+            if (name.CompareTo("VOIntroTrigger") == 0)
+            {
+                Destroy(this.gameObject);
+            }
     }
 }
