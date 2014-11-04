e1a6d28fadd361f24facabaac0fe42716d862c0c
Brittany Waite <bnwbnw3@gmail.com>
2014-11-04 07:01:42 -0700

    script made for theme music player

---
+++ FrailFuture/Assets/Scripts/Managers/ThemeMusicPlayer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThemeMusicPlayer : MonoBehaviour {
+
+    public float multiplier = 0.3f;
+    public static ThemeMusicPlayer self;
+
+    void Awake()
+    {
+        //Edit script Awake call through Edit->ProjectSettings->Script Order
+        if (self == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            self = this;
+        }
+        else if (self != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        audio.volume = GameControl.control.BackgroundMusicVolume * multiplier;
+    }
+    void Update()
+    {
+        audio.volume = GameControl.control.BackgroundMusicVolume * multiplier;
+    }
+}
