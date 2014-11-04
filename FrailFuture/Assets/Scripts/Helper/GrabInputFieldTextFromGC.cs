efffbad2aaa5911df4ca6fe57c5e8ae54a186f99
Brittany Waite <bnwbnw3@gmail.com>
2014-11-04 07:02:53 -0700

    Script to update filename user last used for loading and saving

---
+++ FrailFuture/Assets/Scripts/Helper/GrabInputFieldTextFromGC.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GrabInputFieldTextFromGC : MonoBehaviour {
+
+	// Use this for initialization
+    void Awake()
+    {
+        InputField field = this.gameObject.GetComponent<InputField>();
+        field.value = GameControl.control.LastKnownFileName;
+        field.text.text = GameControl.control.LastKnownFileName;
+    }
+}
