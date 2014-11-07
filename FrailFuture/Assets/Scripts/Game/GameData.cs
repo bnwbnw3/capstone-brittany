bb818f686a4b18fc6b3c8036377c00b5053e8a4b
Brittany Waite <bnwbnw3@gmail.com>
2014-11-07 02:28:12 -0700

    Game data to replace AiData

---
+++ FrailFuture/Assets/Scripts/Game/GameData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+[Serializable]
+public class GameData
+{
+    public float neutrality;
+    public int currentGraphIndex;
+    public int gameLongevity;
+    public int currentPlayThrough;
+    public AIEndingsScore score;
+    public Brain brain;
+    public MazeGroup mazeInfo;
+}
