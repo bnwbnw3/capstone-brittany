23f8c026069e591d4d398e8ce32e2abd04b2c2e8
Brittany Waite <bnwbnw3@gmail.com>
2014-11-16 09:55:40 -0700

    Added in a simple mainmenu script that just shows button to return to main menu

---
+++ FrailFuture/Assets/Scripts/GUI/SimpleMainMenu.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleMainMenu : MonoBehaviour
+{
+    public Menu CurrentMenu;
+    // Use this for initialization
+    void Start()
+    {
+        Screen.lockCursor = false;
+        ShowMenu(CurrentMenu);
+    }
+
+    public void ShowMenu(Menu menu)
+    {
+        if (CurrentMenu != null)
+        {
+            CurrentMenu.IsOpen = false;
+        }
+
+        CurrentMenu = menu;
+        CurrentMenu.IsOpen = true;
+    }
+
+    public void QuitMenus()
+    {
+        Application.Quit();
+    }
+
+    public void MainMenu()
+    {
+        ScreenFader.screenFader.makeSolid("MainMenu", 2.0f);
+    }
+}
