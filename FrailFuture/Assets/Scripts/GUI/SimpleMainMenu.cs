2bfbfa9626e33d73d225a8641f6472c6fbd4c241
Brittany Waite <bnwbnw3@gmail.com>
2014-11-18 03:14:31 -0700

    updated simple menu to edit narration to fade out if return to menu. Put in narration sound (Done by Anthon for now)

--- FrailFuture/Assets/Scripts/GUI/SimpleMainMenu.cs
+++ FrailFuture/Assets/Scripts/GUI/SimpleMainMenu.cs
@@ -4,11 +4,30 @@ using System.Collections;
 public class SimpleMainMenu : MonoBehaviour
 {
     public Menu CurrentMenu;
+    public AudioSource narration;
+    private bool turnNarationOff;
     // Use this for initialization
     void Start()
     {
         Screen.lockCursor = false;
+        turnNarationOff = false;
         ShowMenu(CurrentMenu);
+    }
+
+    void Update()
+    {
+        if (turnNarationOff)
+        {
+            if(narration.volume > 0)
+            {
+                float newVol = 0.5f * Time.deltaTime;
+                narration.volume  -= newVol;
+            }
+            else
+            {
+                narration.Stop();
+            }
+        }
     }
 
     public void ShowMenu(Menu menu)
@@ -29,6 +48,7 @@ public class SimpleMainMenu : MonoBehaviour
 
     public void MainMenu()
     {
+        turnNarationOff = true;
         ScreenFader.screenFader.makeSolid("MainMenu", 2.0f);
     }
 }
---
+++ FrailFuture/Assets/Scripts/Sound/SetIntroCineAudio.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetIntroCineAudio : MonoBehaviour {
+    public float waitTillPlayAudio = 2.0f;
+    public float multiplier = 0.5f;
+    private bool hasStarted;
+	// Use this for initialization
+	void Start () {
+        audio.volume = GameControl.control.BackgroundMusicVolume * multiplier;
+        hasStarted = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!hasStarted)
+        {
+            StartCoroutine(WaitToPlay());
+        }
+	}
+
+    IEnumerator WaitToPlay()
+    {
+        hasStarted = true;
+        yield return new WaitForSeconds(waitTillPlayAudio);
+        audio.volume = GameControl.control.BackgroundMusicVolume * multiplier;
+        audio.Play();
+    }
+}
