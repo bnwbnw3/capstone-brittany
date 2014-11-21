4f77eefea31932d2f319181b0ae81fefdac4a017
Brittany Waite <bnwbnw3@gmail.com>
2014-11-21 13:56:51 -0700

    used to make Vo only move when "on" and not "off"

---
+++ FrailFuture/Assets/Scripts/AnimationControls/UpdateLookBySoundManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UpdateLookBySoundManager : MonoBehaviour
+{
+
+    public LookAtTarget scriptConnected;
+
+    void Update()
+    {
+        scriptConnected.CanUpdate = SoundManager.soundManager.getAi_IsTalking(); ;
+    }
+}
