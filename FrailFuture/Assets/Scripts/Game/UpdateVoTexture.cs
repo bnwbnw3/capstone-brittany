b32778f7cd0bd50758f2cf79228cef32ac01fcae
Brittany Waite <bnwbnw3@gmail.com>
2014-11-21 13:56:28 -0700

    This new Class changes Vo's texture to reflect when it's on or off

---
+++ FrailFuture/Assets/Scripts/Game/UpdateVoTexture.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UpdateVoTexture : MonoBehaviour {
+
+    public Material OnTexture, OffTexuture;
+
+	void Update () 
+    {
+       this.renderer.material = SoundManager.soundManager.getAi_IsTalking()? OnTexture:OffTexuture;
+	}
+}
