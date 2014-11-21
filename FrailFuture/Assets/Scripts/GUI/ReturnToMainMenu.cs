4b66715ca4a683019d65158541ea54fcbd963e5a
Brittany Waite <bnwbnw3@gmail.com>
2014-11-21 13:57:23 -0700

    Used in Intro Cinema destroyed_city to return to the menu after the cinema is over.

---
+++ FrailFuture/Assets/Scripts/GUI/ReturnToMainMenu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReturnToMainMenu : MonoBehaviour {
+
+    public RailPointManager RPM;
+    public float secondsToDelayLoadMainMenu = 1.0f;
+    private bool currentlyLoading;
+
+    void Start()
+    {
+        Screen.lockCursor = true;
+        currentlyLoading = false;
+    }
+
+	void Update () 
+    {
+        if (RPM.AtEndRailPoint && !currentlyLoading)
+        {
+            StartCoroutine(WaitToLoad());
+        }
+	}
+
+    private IEnumerator WaitToLoad()
+    {
+        currentlyLoading = true;
+        yield return new WaitForSeconds(secondsToDelayLoadMainMenu);
+        ScreenFader.screenFader.makeSolid("MainMenu", 2.0f);
+
+    }
+}
